302768b8fd0f7a5fafe18c67801c3efd49c08e47
abhikrit1212 <abhikrit1212@gmail.com>
2021-03-02 11:08:47 +0530

    Three Sensors

--- app/src/main/java/com/example/sidechannelattack/MainActivity.java
+++ app/src/main/java/com/example/sidechannelattack/MainActivity.java
@@ -24,15 +24,15 @@ public class MainActivity
     // TextViews to display current sensor values
     private TextView mTextSensorLight;
     private TextView mTextSensorProximity;
+    private TextView mTextSensorTemperature;
     Intent i;
     private BroadcastReceiver myBroadcastReceiver = new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context context, Intent intent) {
-
                     String sensor_error = getResources().getString(R.string.error_no_sensor);
                     String mSensorLight = intent.getStringExtra("light");
                     String mSensorProximity = intent.getStringExtra("proximity");
-//                    System.out.println(mSensorProximity);
+                    String mSensorTemperature = intent.getStringExtra("temperature");
                     if (mSensorLight == null) {
                         mTextSensorLight.setText(sensor_error);
                     }
@@ -49,36 +49,28 @@ public class MainActivity
                         mTextSensorProximity.setText(getResources().getString(
                                 R.string.label_proximity, p));
                     }
+                    if (mSensorTemperature == null) {
+                        mTextSensorTemperature.setText(sensor_error);
+                    }
+                    else{
+                        Float x= Float.valueOf(mSensorTemperature).floatValue();
+                        mTextSensorTemperature.setText("Temperature Sensor:"+String.valueOf(x));
+                    }
                 }
             };
-//    @Override
-//    public void onResume(){
-//        super.onResume();
-//        IntentFilter intentFilter = new IntentFilter();
-//        intentFilter.addAction(MyService.MY_ACTION);
-//        startService(i);
-//        registerReceiver(myBroadcastReceiver, intentFilter);
-//
-//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextSensorLight = (TextView) findViewById(R.id.label_light);
         mTextSensorProximity = (TextView) findViewById(R.id.label_proximity);
-//        if (ContextCompat.checkSelfPermission(this,
-//                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            String[] permission = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
-//            ActivityCompat.requestPermissions(MainActivity.this, permission, 0);
-//        }
+        mTextSensorTemperature=(TextView) findViewById(R.id.label_temp);
         if (ContextCompat.checkSelfPermission(
                 this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
                 PackageManager.PERMISSION_GRANTED) {
             i = new Intent(MainActivity.this, MyService.class);
             startService(i);
             registerReceiver(myBroadcastReceiver, new IntentFilter(MyService.MY_ACTION));
-
         } else {
             // You can directly ask for the permission.
             String[] permission = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
@@ -87,13 +79,5 @@ public class MainActivity
         i = new Intent(MainActivity.this, MyService.class);
         startService(i);
         registerReceiver(myBroadcastReceiver, new IntentFilter(MyService.MY_ACTION));
-
-//    public void onPause(){
-//        super.onPause();
-//        stopService(i);
-//        if (myBroadcastReceiver != null)unregisterReceiver(myBroadcastReceiver);
-//    }
-
-
     }
 }
--- app/src/main/java/com/example/sidechannelattack/MyService.java
+++ app/src/main/java/com/example/sidechannelattack/MyService.java
@@ -1,5 +1,4 @@
 package com.example.sidechannelattack;
-
 import android.Manifest;
 import android.app.Service;
 import android.content.Context;
@@ -11,26 +10,26 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.util.Log;
-
-
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import com.opencsv.CSVWriter;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
 public class MyService extends Service implements SensorEventListener {
     private SensorManager mSensorManager;
-    // Individual light and proximity sensors.
     private Sensor mSensorProximity;
     private Sensor mSensorLight;
+    private Sensor mSensorTemperature;
     private static float currentLightValue = 0;
+    private static float currentProxValue=0;
+    private static float currentTempValue=0;
+    private static String fileName;
+    private static String filePath;
+    private static File f;
     final static String MY_ACTION = "com.example.sidechannelattack.MyService.MY_ACTION";
     Intent intent = new Intent(MY_ACTION);
     @Nullable
@@ -47,30 +46,29 @@ public class MyService extends Service implements SensorEventListener {
        mSensorProximity =
                mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-
+       mSensorTemperature=mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
    }
     @Override
     public void onSensorChanged(SensorEvent event) {
         String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-
         int sensorType = event.sensor.getType();
         float currentValue = event.values[0];
         long time = event.timestamp;
-        if (currentValue == currentLightValue) return;
         switch (sensorType) {
             // Event came from the light sensor.
             case Sensor.TYPE_LIGHT:
-                String fileName = "Light.csv";
-                String filePath = baseDir + File.separator + fileName;
+                if (currentValue == currentLightValue) return;
+                fileName = "Light.csv";
+                filePath = baseDir + File.separator + fileName;
                 currentLightValue = currentValue;
-                File f = new File(filePath);
+                f = new File(filePath);
                 try {
                     FileWriter fOut = new FileWriter(filePath, true);
                     CSVWriter writer = new CSVWriter(fOut);
 
                     String light = Float.toString(currentValue);
                     intent.putExtra("light", light);
-                    System.out.println("Light value is " + currentValue + " for time: " + time);
+//                    System.out.println("Light value is " + currentValue + " for time: " + time);
 
                     String[] data = { light , Long.toString(time)};
                     writer.writeNext(data, false);
@@ -82,10 +80,42 @@ public class MyService extends Service implements SensorEventListener {
 
                 break;
             case Sensor.TYPE_PROXIMITY:
-                String proxy=Float.toString(currentValue);
-                intent.putExtra("proximity", proxy);
-//                System.out.println("Proxy value is" + currentValue);
-                break;
+                if (currentValue == currentProxValue) return;
+                fileName = "Proximity.csv";
+                filePath = baseDir + File.separator + fileName;
+                currentProxValue = currentValue;
+                f = new File(filePath);
+                try {
+                    FileWriter fOut = new FileWriter(filePath, true);
+                    CSVWriter writer = new CSVWriter(fOut);
+                    String prox= Float.toString(currentValue);
+                    intent.putExtra("proximity", prox);
+//                    System.out.println("Light value is " + currentValue + " for time: " + time);
+                    String[] data = { prox, Long.toString(time)};
+                    writer.writeNext(data, false);
+                    writer.close();
+                } catch (Exception e) {
+
+                }
+            case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                if (currentValue == currentTempValue)
+                    return;
+                fileName = "Temperature.csv";
+                filePath = baseDir + File.separator + fileName;
+                currentTempValue = currentValue;
+                f = new File(filePath);
+                try {
+                    FileWriter fOut = new FileWriter(filePath, true);
+                    CSVWriter writer = new CSVWriter(fOut);
+                    String temp = Float.toString(currentValue);
+                    intent.putExtra("temperature", temp);
+//                    System.out.println("Light value is " + currentValue + " for time: " + time);
+                    String[] data = { temp , Long.toString(time)};
+                    writer.writeNext(data, false);
+                    writer.close();
+                } catch (Exception e) {
+
+                }
             default:
                 // do nothing
         }
@@ -97,7 +127,8 @@ public class MyService extends Service implements SensorEventListener {
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
-    public int onStartCommand(Intent intent, int flags, int startId){
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
         Log.d("MyService", "onStartCommand");
         if (mSensorProximity != null) {
             mSensorManager.registerListener(this, mSensorProximity,
@@ -106,6 +137,10 @@ public class MyService extends Service implements SensorEventListener {
         if (mSensorLight != null) {
             mSensorManager.registerListener(this, mSensorLight,
                     SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if(mSensorTemperature!=null)
+        {
+            mSensorManager.registerListener(this,mSensorTemperature,SensorManager.SENSOR_DELAY_NORMAL);
         }
         return START_STICKY;
     }
