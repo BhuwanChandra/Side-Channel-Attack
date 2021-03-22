62140c65342c1af1cd8922145010096397ab13c7
abhikrit1212 <abhikrit1212@gmail.com>
2021-03-23 00:42:42 +0530

    All Sensors

--- app/src/main/java/com/example/sidechannelattack/MyService.java
+++ app/src/main/java/com/example/sidechannelattack/MyService.java
@@ -20,6 +20,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
+import java.util.*;
 public class MyService extends Service implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mSensorProximity;
@@ -27,32 +28,44 @@ public class MyService extends Service implements SensorEventListener {
     private Sensor mSensorTemperature;
     private Sensor mSensorPressure;
     private static float currentLightValue = 0;
-    private static float currentProxValue=0;
-    private static float currentTempValue=0;
-    private static float currentPressValue=0;
+    private static float currentProxValue = 0;
+    private static float currentTempValue = 0;
+    private static float currentPressValue = 0;
     private static String fileName;
     private static String filePath;
     private static File f;
     final static String MY_ACTION = "com.example.sidechannelattack.MyService.MY_ACTION";
+    String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+    HashMap<String, Float> prev = new HashMap<String, Float>();
+    List<Sensor> sensorsList = new ArrayList<Sensor>();
     Intent intent = new Intent(MY_ACTION);
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
     @Override
-    public void onCreate()
-    {
-       super.onCreate();
-       Log.d("MyService", "onCreate");
-       mSensorManager = (SensorManager) getSystemService(
-               Context.SENSOR_SERVICE);
-       mSensorProximity =
-               mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-       mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-       mSensorTemperature=mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
-       mSensorPressure=mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
-   }
+    public void onCreate() {
+        mSensorManager = (SensorManager) getSystemService(
+                Context.SENSOR_SERVICE);
+        List<Sensor> availableSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (int i = 0; i < availableSensors.size(); i++) {
+            Sensor currentSensor = availableSensors.get(i);
+            String sensorName = currentSensor.getName();
+            Sensor sensor=mSensorManager.getDefaultSensor(currentSensor.getType());
+            sensorsList.add(sensor);
+            prev.put(sensorName, 0.0f);
+        }
+        super.onCreate();
+        Log.d("MyService", "onCreate");
+        mSensorProximity =
+                mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        mSensorPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+    }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -66,7 +79,7 @@ public class MyService extends Service implements SensorEventListener {
                 if (Math.abs(currentValue-currentLightValue)<(0.01*currentLightValue))
                     return;
                 fileName = "Light.csv";
-                filePath = baseDir + File.separator + fileName;
+                filePath = baseDir + File.separator+ fileName;
                 currentLightValue = currentValue;
                 f = new File(filePath);
                 try {
@@ -145,6 +158,7 @@ public class MyService extends Service implements SensorEventListener {
                 {
                 }
             default:
+                WriteSensorValue(event);
                 // do nothing
         }
         sendBroadcast(intent);
@@ -152,14 +166,17 @@ public class MyService extends Service implements SensorEventListener {
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy)
-    {
-
-
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("MyService", "onStartCommand");
+        for (int i = 0; i < sensorsList.size(); i++) {
+        Sensor currentSensor=sensorsList.get(i);
+        if(currentSensor!=null)
+            mSensorManager.registerListener(this, currentSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
         if (mSensorProximity != null) {
             mSensorManager.registerListener(this, mSensorProximity,
                     SensorManager.SENSOR_DELAY_NORMAL);
@@ -168,15 +185,38 @@ public class MyService extends Service implements SensorEventListener {
             mSensorManager.registerListener(this, mSensorLight,
                     SensorManager.SENSOR_DELAY_NORMAL);
         }
-        if(mSensorTemperature!=null)
-        {
-            mSensorManager.registerListener(this,mSensorTemperature,SensorManager.SENSOR_DELAY_NORMAL);
+        if (mSensorTemperature != null) {
+            mSensorManager.registerListener(this, mSensorTemperature, SensorManager.SENSOR_DELAY_NORMAL);
         }
-        if(mSensorPressure!=null)
-        {
-            mSensorManager.registerListener(this,mSensorPressure,SensorManager.SENSOR_DELAY_NORMAL);
+        if (mSensorPressure != null) {
+            mSensorManager.registerListener(this, mSensorPressure, SensorManager.SENSOR_DELAY_NORMAL);
         }
         return START_STICKY;
     }
 
+    public void WriteSensorValue(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+        String sensorName = event.sensor.getName();
+        System.out.println("Name is"+sensorName);
+        float prevValue = prev.get(sensorName);
+        float currentValue = event.values[0];
+        long time = event.timestamp;
+        Date date = java.util.Calendar.getInstance().getTime();
+        if (currentValue == prevValue)
+            return;
+        fileName = sensorName + ".csv";
+        filePath = baseDir + File.separator + fileName;
+        prev.put(sensorName, prevValue);
+        f = new File(filePath);
+        try {
+            FileWriter fOut = new FileWriter(filePath, true);
+            CSVWriter writer = new CSVWriter(fOut);
+            String temp = Float.toString(currentValue);
+            String[] data = {temp, Long.toString(time), date.toString()};
+            writer.writeNext(data, false);
+            writer.close();
+        } catch (Exception e) {
+        }
+
+    }
 }
